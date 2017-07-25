acdf10bfdf7e318478abbf18c36dcd39baf35f04
ericlfay <18708116154@163.com>
2017-07-25 11:20:06 +0800

    创建model (#3)

--- mysite/message_board/models.py
+++ mysite/message_board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class MessageBoard(models.Model):
+    '''用户匿名或创名留言'''
+    visitor_name = models.CharField(max_length=30, help_text='访客姓名')
+    visitor_message = models.TextField(help_text='访客留言')
+    message_create_at = models.DateField(auto_now_add=True, help_text='留言时间')
+
+    def __str__(self):
+        return "%s:%s" % (visitor_name, message_create_at)
